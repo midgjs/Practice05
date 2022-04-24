dd7ede6bb68821b9d4793e9c79fcc3cfbeee840d
midgjs <sjqksk.10@gmail.com>
2022-04-24 17:10:45 +0900

    2022-04-24

---
+++ src/com/javaex/ex02/Goods.java
@@ -0,0 +1,42 @@
+package com.javaex.ex02;
+
+public class Goods {
+	//필드
+	private String name;
+	private int price;
+	
+	//생성자
+	public Goods() {
+	//다른생성자를 사용했을 때 기본생성자를 사용하려면 기본생성자 써줘야한다	
+	}
+	
+	public Goods(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//메소드 gs
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void showInfo() {
+		System.out.println("상품명:" + name + ", 가격:" + price);
+	}
+}
+
+
+
+
